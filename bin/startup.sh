60abfdb1d48e547aedc981dd3857716e83390099
Dan White <dwhite618@gmail.com>
2022-11-12 15:11:14 -0800

    Modified startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,5 +5,5 @@ echo "startup.sh" > /tmp/running_startup.sh.txt
 env | grep DISPLAY >> /tmp/running_startup.sh.txt
 /usr/bin/lxterminal --geometry=120x25 -e bash -c "while [[ 1 ]]; do ping -i 2 www.google.com; sleep 10; done" >& /tmp/errors1.txt || echo "failed ping" >> /tmp/running_startup.sh.txt &
 sleep 2
-/usr/bin/lxterminal -e /bin/bash -c "while [[ 1 ]]; do ~/bin/run_fast_com.py; sleep 10; done" >& /tmp/errors2.txt || echo "failed fast" >> /tmp/running_startup.sh.txt &
+/usr/bin/lxterminal -e /bin/bash -c "while [[ 1 ]]; do uptime; ~/bin/run_fast_com.py; sleep 10; done" >& /tmp/errors2.txt || echo "failed fast" >> /tmp/running_startup.sh.txt &
 
