2855d8fbf96af296773e178bf117024c81e1a8ea
dwhite618 <dwhite618@gmail.com>
2023-01-02 16:14:56 -0800

    Added christmas music loop

--- bin/run_fast_com.py
+++ bin/run_fast_com.py
@@ -7,9 +7,9 @@ for i in range(2):
     print(f"Test {cnt}")
     driver = webdriver.Chrome()
     try:
-        driver.get("https://fast.com")
+        driver.get("https://www.youtube.com/watch?v=8WuKQfCaUb4")
     except:
-        print("Caught exception, unable to open fast.com")
+        print("Caught exception, unable to open youtube.com")
         driver.quit()
     sleep(180)
     cnt+=1
--- bin/run_fast_com.sh
+++ bin/run_fast_com.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
+#while [[ 1 ]]; do
+#    # Carpenters
+#    (sleep 120 && kill -9 $(pgrep chromium-browse))&
+#    /usr/lib/chromium-browser/chromium-browser --autoplay-policy=no-user-gesture-required --app="https://youtu.be/QLK9-fBEgFE
+#    # Vince
+#    (sleep 120 && kill -9 $(pgrep chromium-browse))&
+#    /usr/lib/chromium-browser/chromium-browser --autoplay-policy=no-user-gesture-required --app="https://youtu.be/s9kma0diE3s
+#done
+
 while [[ 1 ]]; do
-    ~/bin/run_fast_com.py
-    for n in $(seq 1 20); do
-        echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
-        sleep 0.1
-        echo 1 | sudo tee /sys/class/leds/led0/brightness > /dev/null
-        sleep 0.1
-    done
-    kill -9 $(pgrep chromium-browse)
-    sleep 10
+    # Carpenters
+    #(sleep 60 && kill -9 $(pgrep chromium-browse))&
+    (sleep 2980 && kill -9 $(pgrep chromium-browse))&
+    /usr/lib/chromium-browser/chromium-browser --autoplay-policy=no-user-gesture-required --app="https://youtu.be/QLK9-fBEgFE?t=0"
+    
+    # Vince
+    #(sleep 60 && kill -9 $(pgrep chromium-browse))&
+    (sleep 3000 && kill -9 $(pgrep chromium-browse))&
+    /usr/lib/chromium-browser/chromium-browser --autoplay-policy=no-user-gesture-required --app="https://youtu.be/s9kma0diE3s?t=0"
+    
+    # Bing Crosby 12 hrs
+    # (sleep 60 && kill -9 $(pgrep chromium-browse))&
+    #(sleep 43000 && kill -9 $(pgrep chromium-browse))&
+    #/usr/lib/chromium-browser/chromium-browser --autoplay-policy=no-user-gesture-required --app="https://youtu.be/8WuKQfCaUb4?t=0"
+    
+    # Frank Sinatra 
+    # (sleep 60 && kill -9 $(pgrep chromium-browse))&
+    (sleep 3300 && kill -9 $(pgrep chromium-browse))&
+    /usr/lib/chromium-browser/chromium-browser --autoplay-policy=no-user-gesture-required --app="https://youtu.be/ob2GrSTjVLc?t=0"
+
+
 done
+
+
+#https://youtu.be/QLK9-fBEgFE?t=682
